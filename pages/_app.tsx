858157e7753f1d97971e2f73002ae25b1e9468b9
samuraikun <tazitaziawawa@gmail.com>
2020-04-04 23:52:05 +0900

    And Design追加 & And DesinをグローバルCSSとして適用するために、pages/_app.tsx作成

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import 'antd/dist/antd.css'
+import { AppProps } from 'next/app'
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+)
+
+export default MyApp
