89d0561b71ac9ae41625c1cfeb78545d84a6f287
samuraikun <tazitaziawawa@gmail.com>
2020-04-05 20:45:20 +0900

    Root Component is wrapped by Apollo Client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
 import 'antd/dist/antd.css'
 import { AppProps } from 'next/app'
+import { withApollo } from '../components/WithApolloClient'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
 )
 
-export default MyApp
+export default withApollo({ ssr: true })(MyApp)
