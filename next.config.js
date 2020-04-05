261520ffd44f36df26867ceb3ae11140207a61f1
samuraikun <tazitaziawawa@gmail.com>
2020-04-05 19:42:47 +0900

    add graphcms setting & apollo packages

--- next.config.js
+++ next.config.js
@@ -1,2 +1,26 @@
-const withCSS = require('@zeit/next-css');
-module.exports = withCSS({})
+const withCSS = require('@zeit/next-css')
+require('dotenv').config()
+
+const {
+  APIURL,
+  APIKEY,
+  PROJECTID,
+  BRANCH,
+  CDNBASE,
+  GRAPHCMSID,
+  GRAPHCMSURL,
+} = process.env
+
+module.exports = withCSS({
+  publicRuntimeConfig: {
+    graphcms: {
+      APIURL,
+      APIKEY,
+      PROJECTID,
+      BRANCH,
+      CDNBASE,
+      GRAPHCMSID,
+      GRAPHCMSURL,
+    },
+  }
+})
